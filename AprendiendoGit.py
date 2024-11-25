0fd9055c5ce6163d02e5b54b1ae8e29c2a1e16f5
ariel <aribraver3@hotmail.com>
2024-11-25 17:47:30 -0300

    Tercer commit rama 1
    Empiezo a usar GitKraken

--- AprendiendoGit.py
+++ AprendiendoGit.py
@@ -1,3 +1,4 @@
 print("holash")
 print("ahora hago el tercer commit pero en una rama nueva")
 print("ahor hago un nuevo commit en esta rama, haciendo que sea el segundo de la misma")
+print("empiezo a usar GitKraken")
